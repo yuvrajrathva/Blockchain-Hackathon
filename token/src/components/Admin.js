1a0b0447e92f61414c8b1bb01e6b69b39e7c860e
Yuvraj Rathva <rathva.1@iitj.ac.in>
2023-01-26 02:53:26 +0530

    adding admin and student pages

--- token/src/components/Admin.js
+++ token/src/components/Admin.js
@@ -1,6 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import "./Admin.css";
 function Admin() {
-  return <h1>This is Admin page</h1>;
+  return (
+    <>
+      <div className="admin-container">
+        <form>
+          <h1>ISSUE TOKENS</h1>
+          <label for="username">Address</label>
+          <input
+            type="text"
+            placeholder="Enter Address of student"
+            id="username"
+          />
+          <label for="password">Tokens</label>
+          <input type="password" placeholder="Token Numbers" id="password" />
+          <button>ISSUE</button>
+        </form>
+      </div>
+    </>
+  );
 }
 export default Admin;
--- token/src/components/Student.js
+++ token/src/components/Student.js
@@ -1,5 +1,26 @@
 import { Route, Routes } from "react-router-dom";
+import "./Student.css";
+
 function Student() {
-  return <h1>This is Student page</h1>;
+  return (
+    <>
+      <div className="student-container">
+        <form>
+          <h1>PAYMENT</h1>
+          <label for="username">Pay to</label>
+          <input
+            type="text"
+            placeholder="Enter your recipient's Address"
+            id="username"
+          />
+          <label for="password">Tokens</label>
+          <input type="password" placeholder="Token Numbers to send" id="password" />
+          <button>PAY to Student</button>
+          <p>or</p>
+          <button>PAY to Vendor</button>
+        </form>
+      </div>
+    </>
+  );
 }
 export default Student;
